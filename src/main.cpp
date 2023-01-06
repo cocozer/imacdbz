78ed1291f822b2a7a658d1acdad986e6e12c483e
cocozer <corentin.pak@gmail.com>
2023-01-06 13:57:41 +0100

    premier commit priere

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include "application_ui.h"
+#include "SDL2_gfxPrimitives.h"
+
+
+void draw(SDL_Renderer* renderer)
+{
+    /* Remplissez cette fonction pour faire l'affichage du jeu */
+    
+    filledEllipseRGBA(
+        renderer,
+        50,
+        50,
+        15,
+        15,
+        255,
+        0,
+        0,
+        255
+    ); 
+    ellipseRGBA(renderer, 
+        50,
+        50,
+        15,
+        15,
+        255,
+        255,
+        255,
+        255);
+
+    /*SDL2_GFXPRIMITIVES_SCOPE int filledEllipseRGBA(SDL_Renderer * renderer, Sint16 x, Sint16 y,
+		Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a);*/
+   
+}
+
+bool handleEvent()
+{
+    /* Remplissez cette fonction pour gérer les inputs utilisateurs */
+    SDL_Event e; 
+    while (SDL_PollEvent(&e)){ 
+        if (e.type == SDL_QUIT) 
+            return false; 
+    }
+    return true;
+}
+
+
+int main(int argc, char** argv) {
+    SDL_Window* gWindow;
+    SDL_Renderer* renderer;
+    bool is_running = true; 
+
+    // Creation de la fenetre
+    gWindow = init("Awesome Game");
+
+    if (!gWindow)
+    {
+        SDL_Log("Failed to initialize!\n");
+        exit(1);
+    }
+
+    renderer = SDL_CreateRenderer(gWindow, -1, 0); // SDL_RENDERER_PRESENTVSYNC
+
+    /*  GAME LOOP  */
+    while(true)
+    {
+        // INPUTS
+        is_running = handleEvent();
+        if (!is_running)
+            break;
+
+        // GESTION ACTEURS
+
+        // ...
+        
+        // EFFACAGE FRAME
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        
+        // DESSIN
+        draw(renderer);
+
+        // VALIDATION FRAME
+        SDL_RenderPresent(renderer);
+
+        // PAUSE en ms
+        SDL_Delay(1000/30); 
+    }
+
+    //Free resources and close SDL
+    close(gWindow, renderer);
+
+    return 0;
+}
