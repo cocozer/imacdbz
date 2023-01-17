26e7d06717801a23cb3d1e6286c6ddab40de72eb
cocozer <corentin.pak@gmail.com>
2023-01-17 22:52:14 +0100

    commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,12 @@
 #include <cstdlib>
 using namespace std;
 // Fonctions
-
-    int randomNumber(int min, int max)
-    {
+// Fonction qui à partir d'un minimum et d'un maximum, renvoie un nombre aléatoire
+int randomNumber(int min, int max){
         return min + (rand() % max);
-    }
+}
 // STRUCT VECTOR 2 FONCTIONS
-
+// Fonctions qui retournent les valeurs du Vecteur2
 int Vector2::returnx()
 {
     return this->x;
@@ -22,6 +21,7 @@ int Vector2::returny()
     return this->y;
 }
 
+//Fonction d'écriture d'un Vecteur2
 void Vector2::writeVector2(int x, int y)
 {
     this->x = x;
@@ -30,6 +30,7 @@ void Vector2::writeVector2(int x, int y)
 
 // STRUCT VECTOR 3 FONCTIONS
 
+// Fonctions qui retournent les valeurs du Vecteur3
 int Vector3::returnr()
 {
     return this->r;
@@ -44,7 +45,7 @@ int Vector3::returnb()
 {
     return this->b;
 }
-
+// Fonction qui écrit et assigne les valeurs du Vecteur 3
 void Vector3::writeVector3(unsigned int r, unsigned int g, unsigned int b)
 {
     this->r = r;
@@ -52,6 +53,8 @@ void Vector3::writeVector3(unsigned int r, unsigned int g, unsigned int b)
     this->b = b;
 }
 
+// Fonctions de la structure WALL
+// Fonction qui assigne et écrit les valeurs d'un mur rectanglaire (mur extérieur)
 void Wall::writeWall(int x1, int y1, int x2, int y2)
 {
     this->x1 = x1;
@@ -60,20 +63,25 @@ void Wall::writeWall(int x1, int y1, int x2, int y2)
     this->y2 = y2;
 }
 
+//Fonction qui écrit et assigne les valeurs d'un mur interne
 void internWall::writeInternWall(bool horizontal, int taille, Vector2 position)
 {
     this->horizontal = horizontal;
     this->taille = taille;
     this->position = position;
 }
+// Fonction qui dessine un mur interne
 void internWall::draw(SDL_Renderer *renderer) {
+
+    //Si le mur est horizontal, on appelle la fonction qui dessine une ligne horizontale
     if (this->horizontal == true) {
         hlineRGBA(renderer, this->position.x, this->position.x+this->taille, this->position.y, 255, 255, 255, 255);
-    } else {
+    } else { // Sinon, on appelle celle de la ligne verticale
         vlineRGBA(renderer, this->position.x, this->position.y, this->position.y+this->taille, 255, 255, 255, 255);
     }
 }
 // STRUCT BALL FONCTIONS
+// Fonction qui assigne et écrit les valeurs d'une balle
 void Ball::writeBall(int x, int y, int vx, int vy, int rayon, unsigned int r, unsigned int g, unsigned int b, unsigned int br, unsigned int bg, unsigned int bb)
 {
     this->position.writeVector2(x, y);
@@ -82,25 +90,27 @@ void Ball::writeBall(int x, int y, int vx, int vy, int rayon, unsigned int r, un
     this->color.writeVector3(r, g, b);
     this->bordercolor.writeVector3(br, bg, bb);
 }
+// Fonction qui dessine une balle
 void Ball::draw(SDL_Renderer *renderer)
 {
     /* Remplissez cette fonction pour faire l'affichage du jeu */
-
+    // on dessine la balle 
     filledEllipseRGBA(renderer, this->position.returnx(), this->position.returny(), this->radius.returnx(), this->radius.returny(), this->color.returnr(), this->color.returng(), this->color.returnb(), 255);
-    ellipseRGBA(renderer, this->position.returnx(), this->position.returny(), this->radius.returnx(), this->radius.returny(), this->bordercolor.returnr(), this->bordercolor.returng(), this->bordercolor.returnb(), 255);
+    // on dessine l'anneau de la balle pour que ça ressemble à une planète comme saturne lol !!!
+    ellipseRGBA(renderer, this->position.returnx(), this->position.returny(), this->radius.returnx()+30, this->radius.returny()-10, this->bordercolor.returnr(), this->bordercolor.returng(), this->bordercolor.returnb(), 255);
 
-    /*SDL2_GFXPRIMITIVES_SCOPE int filledEllipseRGBA(SDL_Renderer * renderer, Sint16 x, Sint16 y,
-        Sint16 rx, Sint16 ry, Uint8 r, Uint8 g, Uint8 b, Uint8 a);*/
 }
+// Fonction qui met à jour la position de la balle en additionant la velocité (qui comprend la direction et la vitesse) à la position pour définir la nouvelle position
 void Ball::updatePosition()
 {
     this->position.x += this->velocity.x;
     this->position.y += this->velocity.y;
 }
+// Fonction qui vérifie si une balle heurte un mur extérieur. Si c'est le cas, on inverse la vélocité de la balle sur l'axe qui a touché le mur pour la faire rebondir
 void Ball::checkWalls(Wall wall)
 {
 
-    // Si la balle touche un bord, sa vitesse est inversée
+    // Si la balle touche un bord, sa vitesse dans l'axe concerné est inversée
     if (this->position.x - this->radius.x <= wall.x1)
     {
         this->velocity.x = -this->velocity.x;
@@ -118,6 +128,7 @@ void Ball::checkWalls(Wall wall)
         this->velocity.y = -this->velocity.y;
     }
 }
+// Fonction qui vérifie si une balle heurte une autre balle. Si c'est le cas, on échange les vitesses des deux balles pour les faire rebondir entre elles
 void Ball::checkBalls(Ball *balls)
 {
     // On parcourt toutes les balles
@@ -144,6 +155,7 @@ void Ball::checkBalls(Ball *balls)
         otherball = otherball->next;
     }
 }
+//On crée une nouvelle balle aux coordonnées données
 Ball *newBallWithPos(Ball *list, int x, int y) {
     Ball *new_ball = new Ball;
     if (new_ball == nullptr)
@@ -156,12 +168,12 @@ Ball *newBallWithPos(Ball *list, int x, int y) {
 
     // Première génération d'un nombre aléatoire pour décider si la vélocité sera positive ou négative
     // Puis ensuite, si c'est négatif on génère une vélocité négative. sinon positive.
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.x = randomNumber(-7, 7);
     }   else {
         new_ball->velocity.x = randomNumber(7, 7);
     }
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.y = randomNumber(-7, 7);
     }   else {
         new_ball->velocity.y = randomNumber(7, 7);
@@ -184,6 +196,7 @@ Ball *newBallWithPos(Ball *list, int x, int y) {
     else
         return new_ball;
 }
+// Création d'une balle avec une couleur donnée
 Ball *newBallWithColor(Ball *list, int r, int g, int b) {
     Ball *new_ball = new Ball;
     if (new_ball == nullptr)
@@ -196,12 +209,12 @@ Ball *newBallWithColor(Ball *list, int r, int g, int b) {
 
     // Première génération d'un nombre aléatoire pour décider si la vélocité sera positive ou négative
     // Puis ensuite, si c'est négatif on génère une vélocité négative. sinon positive.
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.x = randomNumber(-7, 7);
     }   else {
         new_ball->velocity.x = randomNumber(7, 7);
     }
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.y = randomNumber(-7, 7);
     }   else {
         new_ball->velocity.y = randomNumber(7, 7);
@@ -224,6 +237,7 @@ Ball *newBallWithColor(Ball *list, int r, int g, int b) {
     else
         return new_ball;
 }
+// Création d'une balle totalement aléatoire
 Ball *newBall(Ball *list)
 {
     Ball *new_ball = new Ball;
@@ -236,12 +250,12 @@ Ball *newBall(Ball *list)
     new_ball->position.y = randomNumber(30, 410);
     // Première génération d'un nombre aléatoire pour décider si la vélocité sera positive ou négative
     // Puis ensuite, si c'est négatif on génère une vélocité négative. sinon positive.
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.x = -randomNumber(1, 7);
     }   else {
         new_ball->velocity.x = randomNumber(1, 7);
     }
-    if (randomNumber(0, 1) == 0) {
+    if (randomNumber(1, 2) == 1) {
         new_ball->velocity.y = -randomNumber(1, 7);
     }   else {
         new_ball->velocity.y = randomNumber(1, 7);
@@ -264,7 +278,7 @@ Ball *newBall(Ball *list)
     else
         return new_ball;
 }
-
+// On trouve la dernière balle de la liste chainée et on la renvoie
 Ball *find_last(Ball *list)
 {
     while (list->next != nullptr)
@@ -273,118 +287,75 @@ Ball *find_last(Ball *list)
     }
     return list;
 }
-
+// Fonction qui gère les clics de l'utilisateur
 void handleEvent(Ball **balls)
 {
-
     /* Définition des variables de position de curseur */
     int xMouse;
     int yMouse;
     SDL_Event e;
     while (SDL_PollEvent(&e))
     {
+        // On définit une balle actuelle et la balle qui la précède (ça sert à pouvoir supprimer la balle actuelle)
         Ball *previous = nullptr;
         Ball *current = *balls;
-        // Condition de clic
+        // Si on presse le clic (renvoie une seule information et n'est pas un levier)
         if (e.type == SDL_MOUSEBUTTONDOWN)
         { // lors d'un clic
-            // Capture les positions du curseur à l'instant du clic
+            // On crée une variable qui indique si le clic est sur la balle ou pas. On met cette variable de base sur faux
             bool clickedonball = false;
+            // Capture les positions du curseur à l'instant du clic
             SDL_GetMouseState(&xMouse, &yMouse);
+            // On parcourt toutes les balles
             while (current != nullptr)
             {
-                
-                // for (int i = 0; i<nbBalles;i++){
-
-                // // Au lieu de prendre simplement la position, on divise les valeurs par le rayon pour pouvoir cliquer sur une balle en cliquant sur n'importe quel endroit de sa surface
-                // int yMouseDividedByRadius = yMouse / balles[i].radius.y;
-                // int xMouseDividedByRadius = xMouse / balles[i].radius.x;
-                // int yBallPositionDividedByRadius = balles[i].position.y / balles[i].radius.y;
-                // int xBallPositionDividedByRadius = balles[i].position.x / balles[i].radius.x;
-
+                //Si l'utilisateur a cliqué sur la balle
                 if (yMouse > current->position.y - current->radius.y && yMouse < current->position.y + current->radius.y && xMouse > current->position.x - current->radius.x && xMouse < current->position.x + current->radius.x)
                 {
-                //L'utilisateur a cliqué sur la balle
+
+                    // On indique que la balle a été cliquée à la variable de tout à l'heure
                     clickedonball = true;
+                    // On cherche maintenant à supprimer la balle
+                    // On traite chaque cas : si c'est la première balle de la liste chainée, alors la liste commencera à partir de la deuxième balle
                     if (previous == nullptr) {
                         *balls = current->next;
-                    } else if (find_last(*balls) == current) {
-
+                    } else if (find_last(*balls) == current) { // on traite chaque cas : Si c'est la dernière balle, alors la balle qui la précède n'aura plus de balle suivante
                         previous->next = nullptr;
-                    } else {
+                    } else { // Si aucun des cas spéciaux n'est effectif, on assigne la prochaine balle de la précédente à la prochaine (ce qui supprime celle actuelle)
                         previous->next = current->next;
                     }
                     break;
                 } 
                 // }
+                // On assigne la balle précédente à l'actuelle
                 previous = current;
-                current = current->next;
+                // On assigne la balle actuelle à la prochaine 
+                current = current->next; // Ce qui fait que la balle previous reste celle d'avant
             }
-            // Si l'utilisateur n'a pas cliqué sur la balle, on crée une nouvelle balle
+            // Si l'utilisateur n'a pas cliqué sur la balle, on crée une nouvelle balle à l'endroit cliqué
             if (clickedonball == false) {
                 *balls = newBallWithPos(*balls, xMouse, yMouse);
             }
         }
     }
-    // Ball *previous = nullptr;
-    // Ball *current = *balls;
-    // // Condition de clic
-    // if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(1))
-    // { // lors d'un clic
-    //     // Capture les positions du curseur à l'instant du clic
-    //     bool clickedonball = false;
-    //     SDL_GetMouseState(&xMouse, &yMouse);
-    //     while (current != nullptr)
-    //     {
-            
-    //         // for (int i = 0; i<nbBalles;i++){
-
-    //         // // Au lieu de prendre simplement la position, on divise les valeurs par le rayon pour pouvoir cliquer sur une balle en cliquant sur n'importe quel endroit de sa surface
-    //         // int yMouseDividedByRadius = yMouse / balles[i].radius.y;
-    //         // int xMouseDividedByRadius = xMouse / balles[i].radius.x;
-    //         // int yBallPositionDividedByRadius = balles[i].position.y / balles[i].radius.y;
-    //         // int xBallPositionDividedByRadius = balles[i].position.x / balles[i].radius.x;
-
-    //         if (yMouse > current->position.y - current->radius.y && yMouse < current->position.y + current->radius.y && xMouse > current->position.x - current->radius.x && xMouse < current->position.x + current->radius.x)
-    //         {
-    //            //L'utilisateur a cliqué sur la balle
-    //             clickedonball = true;
-    //             if (previous == nullptr) {
-    //                 *balls = current->next;
-    //             } else if (find_last(*balls) == current) {
-
-    //                 previous->next = nullptr;
-    //             } else {
-    //                 previous->next = current->next;
-    //             }
-    //             break;
-    //         } 
-    //         // }
-    //         previous = current;
-    //         current = current->next;
-    //     }
-    //     // Si l'utilisateur n'a pas cliqué sur la balle, on crée une nouvelle balle
-    //     bool ballspawned = false;
-    //     if (clickedonball == false && ballspawned == false) {
-    //         *balls = newBall(*balls);
-    //     }
-    // }
 }
-
+// Fonction qui fait bouger les balles
 void moveBalls(Ball *balls, Wall wall)
 {
+    // On parcourt toutes les balles.
     Ball *current = balls;
     while (current != nullptr)
     {
-        current->checkBalls(balls);
-        current->checkWalls(wall);
-        current->updatePosition();
+        current->checkBalls(balls); // On vérifie si la balle touche une balle, si oui la fonction fait rebondir les balles entre elles
+        current->checkWalls(wall); // On vérifie si la balle touche un mur extérieur, si oui la fonction fait rebondir la balle sur l'axe touché
+        current->updatePosition(); // On fait bouger la balle vers l'endroit où elle est dirigée à la vitesse qui lui est assignée
         current = current->next;
     }
 }
-
+// Fonction qui dessine toutes les balles
 void draw(Ball *balls, SDL_Renderer *renderer)
 {
+    // On parcourt toutes les balles et on les dessine toutes
     while (balls->next != nullptr)
     {
         balls->draw(renderer);
@@ -392,23 +363,8 @@ void draw(Ball *balls, SDL_Renderer *renderer)
     }
     balls->draw(renderer);
 }
-// bool handleEvent()
-// {
-//     /* Remplissez cette fonction pour gérer les inputs utilisateurs */
-//     SDL_Event e;
-//     while (SDL_PollEvent(&e)){
-//         switch (e.type) {
-//             case SDL_MouseButtonEvent:
-
-//                 break;
-//         }
-//             return false;
-//     }
-//     return true;
-// }
-
-int main(int argc, char **argv)
-{
+// Je vous présente la fonction main Ô_Ô
+int main(int argc, char **argv) {
     SDL_Window *gWindow;
     SDL_Renderer *renderer;
     bool is_running = true;
@@ -423,36 +379,32 @@ int main(int argc, char **argv)
     }
     renderer = SDL_CreateRenderer(gWindow, -1, 0); // SDL_RENDERER_PRESENTVSYNC
 
-    // Initialisation des acteurs
-
-    // Initialisation des balles
-
     // Initialisation des balles
     Ball *balls = nullptr;
-        int nbBalles = 5;
-    if (argc == 2) {
-        int arg = atoi(argv[1]);
-        if (arg > 0) {
-            nbBalles = arg;
+    int nbBalles = 5; // A la base, le nombre de balles est de 5
+    if (argc == 2) { // Si on lance le programme avec un argument
+        int arg = atoi(argv[1]); // on récupère l'argument
+        if (arg > 0) { // Si cet argument n'est pas égal à 0
+            nbBalles = arg; // le nombre de balles change et devient l'argument
         }
-    } else if (argc > 2) {
-        for (int i = 1; i+1 < argc; i++){
+    } else if (argc > 2) { // Si il y a plusieurs arguments 
+        for (int i = 1; i+1 < argc; i++){ // On part de l'argument 1 et on parcourt tous les arguments
             int r;
             int g;
             int b;
-            sscanf(argv[i], "%d,%d,%d", &r, &g, &b);
-            balls = newBallWithColor(balls,r, g, b);
+            sscanf(argv[i], "%d,%d,%d", &r, &g, &b); // on récupère les nombres des couleurs et on les assigne dans les variables r, g et b
+            balls = newBallWithColor(balls,r, g, b); // on crée une balle à partir des couleurs et on l'assigne à la liste chainée balls
         }
     }
 
-    for (int i = 0; i < nbBalles; i++)
+    for (int i = 0; i < nbBalles; i++) // on crée autant de balles qu'il y a dans nbBalles
     {
         balls = newBall(balls);
     }
 
     // Initialisations des murs
-    Wall wall;
-    wall.writeWall(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    Wall wall; // On crée un mur
+    wall.writeWall(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT); // on met le mur autour de la fenêtre
 
     // Initialisation des murs internes
     internWall walls[3];
@@ -486,10 +438,6 @@ int main(int argc, char **argv)
         }
         handleEvent(&balls);
         // GESTION ACTEURS
-        // for(int i=0; i<nbBalles; i++){
-        //     balles[i].checkWalls(wall);
-        //     balles[i].updatePosition();
-        // }
         moveBalls(balls, wall);
         // ...
 
@@ -498,8 +446,6 @@ int main(int argc, char **argv)
         SDL_RenderClear(renderer);
 
         // DESSIN
-        // string var = "salut";
-        // SDL_Log(var.c_str());
         draw(balls, renderer);
 
         // VALIDATION FRAME
